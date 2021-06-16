55ee04a12a30fb3c049a0056e421a1954af31ee4
FranciscoPaixao <26686029+FranciscoPaixao@users.noreply.github.com>
2021-06-16 19:39:44 +0000

    .

--- nomes.js
+++ nomes.js
@@ -16,16 +16,6 @@
     .catch(err => { throw err });
   }
   
-  /*
-  function getNomeAleatorio() {
-  fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://gerador-nomes.herokuapp.com/nome/aleatorio')}`)
-                    .then(response => {
-                      if (response.ok) return response.json()
-                      throw new Error('Network response was not ok.')
-                    })
-                    .then(data => {refreshDisplay(JSON.parse(data.contents)[0]); console.log(JSON.parse(data.contents)[0])});
-  }
-*/
   // Refresh Display
   function refreshDisplay(nome) {
     document.getElementById("nome1").textContent = nome
