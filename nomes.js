84252a28a58736f24fc7eb70df2fd159a2592418
FranciscoPaixao <26686029+FranciscoPaixao@users.noreply.github.com>
2021-06-16 18:19:43 +0000

    .

--- nomes.js
+++ nomes.js
@@ -4,23 +4,17 @@
 
   // Get a random name
   function getNomeAleatorio() {
-
-    let url = 'https://thingproxy.freeboard.io/fetch/https://gerador-nomes.herokuapp.com/nome/aleatorio';
-
-    fetch(url)
-    .then(res => res.json())
-    .then((out) => {
-      // Refresh Display
-      refreshDisplay(out[0], out[1], out[2])
-    })
-    .catch(err => { throw err });
+  fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://gerador-nomes.herokuapp.com/nome/aleatorio')}`)
+                    .then(response => {
+                      if (response.ok) return response.json()
+                      throw new Error('Network response was not ok.')
+                    })
+                    .then(data => {refreshDisplay(JSON.parse(data.contents)[0]); console.log(JSON.parse(data.contents)[0])});
   }
 
   // Refresh Display
-  function refreshDisplay(nome, apelido1, apelido2) {
+  function refreshDisplay(nome) {
     document.getElementById("nome1").textContent = nome
-    document.getElementById("apelido1").textContent = apelido1
-    document.getElementById("apelido2").textContent = apelido2
   }
 
 
@@ -43,6 +37,4 @@
 
     getNomeAleatorio();
     play();
-    enableControls();
-
   })();
