240f805fb4f689cca38dc0b7f0adca029558f435
FranciscoPaixao <26686029+FranciscoPaixao@users.noreply.github.com>
2021-06-16 19:17:15 +0000

    .

--- url.php
+++ url.php
@@ -1,6 +1,10 @@
 <?php
 $url = file_get_contents($_GET['url']);
 
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
 header('Content-Type: '.mime_content_type($_GET['url']));
 
 echo $url;
